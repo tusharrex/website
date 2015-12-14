98bcb2d61990e0d3b82a14b0f393bba4d561084d
Jarek Plonski <jarek.github@gmail.com>
2015-12-14 11:29:52 +0100

    CQW-34 Separate layouts, change config to use directory_indexes, placeholder about page, placeholder footer partial

--- config.rb
+++ config.rb
@@ -1,11 +1,14 @@
 require 'slim'
 require 'lib/custom_helpers'
 
+activate :directory_indexes
+
 ###
 # Blog settings
 ###
 
 # Time.zone = "UTC"
+
 
 activate :blog do |blog|
   blog.prefix = 'blog'
@@ -60,9 +63,16 @@ end
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
+page "/index.html", layout: :landing_page
+
+with_layout :website do
+  page "/about.html"
+end
+
 ###
 # Helpers
 ###
+
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
