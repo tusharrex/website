5b2d0878916f70b828c020dfb66af226b5f386d0
Jarek Plonski <jarek.github@gmail.com>
2015-12-18 12:28:54 +0100

    CQW-54 Remove blog from config.rb

--- config.rb
+++ config.rb
@@ -12,29 +12,6 @@ activate :directory_indexes
 # Time.zone = "UTC"
 
 
-activate :blog do |blog|
-  blog.prefix = 'blog'
-
-  blog.permalink = '{year}/{month}/{day}/{title}.html'
-  blog.sources = '{year}-{month}-{day}-{title}.html'
-  blog.taglink = 'tags/{tag}.html'
-  blog.layout = 'layout'
-  blog.summary_separator = /(READMORE)/
-  blog.summary_length = 250
-  blog.year_link = '{year}.html'
-  blog.month_link = '{year}/{month}.html'
-  blog.day_link = '{year}/{month}/{day}.html'
-  blog.default_extension = '.slim'
-
-  # blog.tag_template = 'tag.html'
-  # blog.calendar_template = 'calendar.html'
-
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
-end
-
 ###
 # Compass
 ###
