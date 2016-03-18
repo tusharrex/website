53b6879c4780bed7656ae0b2305025dab6d8e7be
Magdalena Malinowska <magda@codequest.com>
2016-03-18 13:59:45 +0100

    Change title for blog

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -1,10 +1,14 @@
 module CustomHelpers
   def title_helper
-    return data.page.title if data.page.title
+    return data.page.title if data.page.title && !blog?
     'code quest is a Ruby on Rails and iOS development studio.'
   end
 
   def description_helper
     data.page.description ? data.page.description : ''
+  end
+
+  def blog?
+    current_page.path == 'blog.html'
   end
 end
