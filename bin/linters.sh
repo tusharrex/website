7b529a8eee9975070eb0212db6a04b287a181728
Magdalena Malinowska <magda@codequest.com>
2016-11-02 20:33:03 +0100

    Change linter task

--- bin/linters.sh
+++ bin/linters.sh
@@ -2,7 +2,8 @@
 
 # run linter
 
-bundle exec rubocop
+bundle exec slim-lint -c .slim-lint.yml source
+bundle exec bundle exec ruby -W0 -S rubocop lib spec
+coffeelint -f coffeelint.json source/javascripts/
 bundle exec scss-lint -c .scss-lint.yml source/stylesheets/**/*.scss
-coffeelint -f coffeelint.json source/javascripts/application.js.coffee
-bundle exec slim-lint -c .slim-lint.yml
+
