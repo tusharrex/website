43d187359dde5b85ac997a9c8366cf75b45acb59
Karol Zawadzki <karol@pierwiastek.org>
2016-11-21 10:15:15 +0100

    autoprefixer przeniesiony do konfiguracji builda (#312)

--- config.rb
+++ config.rb
@@ -12,11 +12,6 @@ activate :blog do |blog|
 end
 activate :directory_indexes
 
-activate :autoprefixer do |config|
-  config.browsers = ['last 2 versions', 'Explorer >= 11', 'Safari >= 6']
-  config.ignore   = ['/stylesheets/hacks.css']
-end
-
 page '/index.html', layout: :landing_page
 page '/404.html', layout: :website
 page '/feed.xml', layout: false
@@ -67,6 +62,10 @@ configure :build do
   activate :minify_javascript
   activate :asset_hash
   activate :gzip
+  activate :autoprefixer do |config|
+    config.browsers = ['last 2 versions', 'Explorer >= 11', 'Safari >= 6']
+    config.ignore   = ['/stylesheets/hacks.css']
+  end
 end
 
 helpers CustomHelpers
